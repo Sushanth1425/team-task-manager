ff8fb728fba579c03b1483e72985f37980e512f6
Sushanth1425 <sushanthb1425@gmail.com>
2026-05-20 15:30:14 +0530

    added proj card

--- frontend/src/components/ProjectCard.jsx
+++ frontend/src/components/ProjectCard.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+function ProjectCard({ project, navigate }) {
 
-const ProjectCard = () => {
   return (
-    <div>ProjectCard</div>
-  )
+    <div className="bg-white shadow rounded p-4 cursor-pointer hover:shadow-lg transition" onClick={()=> navigate(`/project/${project._id}`)}>
+      <h2 className="text-xl font-bold">{project.name}</h2>
+      <p className="text-gray-500 mt-2"> Members: {project.members.length}</p>
+    </div>
+  );
 }
 
-export default ProjectCard
+export default ProjectCard;

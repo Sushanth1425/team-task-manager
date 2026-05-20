99ba953721c6e8df1c4a969b7138bba27270598b
Sushanth1425 <sushanthb1425@gmail.com>
2026-05-20 15:32:39 +0530

    added task cark

--- frontend/src/components/TaskCard.jsx
+++ frontend/src/components/TaskCard.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+function TaskCard({ task, updateStatus }) {
 
-const TaskCard = () => {
   return (
-    <div>TaskCard</div>
-  )
+
+    <div className="bg-white shadow rounded p-4">
+      <div className="flex justify-between">
+        <div>
+          <h2 className="text-xl font-bold"> {task.title}</h2>
+          <p className="mt-2">{task.description}</p>
+          <p className="text-sm text-gray-500 mt-2">Priority: {task.priority}</p>
+          <p className="text-sm text-gray-500"> Assigned:{" "} {task.assignedTo?.name || "None"}</p>
+        </div>
+        <select value={task.status} onChange={(e)=> updateStatus( task._id, e.target.value)} className="border p-2 rounded h-fit">
+          <option>To Do</option>
+          <option>In Progress</option>
+          <option>Done</option>
+        </select>
+      </div>
+    </div>
+  );
 }
 
-export default TaskCard
+export default TaskCard;

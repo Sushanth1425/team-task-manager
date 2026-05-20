106ff59a8ecbbd92208b5bef9b1f25108862d3af
Sushanth1425 <sushanthb1425@gmail.com>
2026-05-20 15:28:21 +0530

    added navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,21 @@
-import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Dashboard from "./pages/Dashboard";
+import ProjectPage from "./pages/ProjectPage";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
-
   return (
-    <div> hi </div>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/dashboard" element={ <ProtectedRoute> <Dashboard /> </ProtectedRoute>} />
+        <Route path="/project/:id" element={<ProtectedRoute> <ProjectPage /> </ProtectedRoute>}/>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import { useNavigate } from "react-router-dom";
 
-const Navbar = () => {
+function Navbar() {
+  const navigate = useNavigate();
+  const logout = () => {
+    localStorage.clear();
+    navigate("/");
+  };
+
   return (
-    <div>Navbar</div>
-  )
+    <div className="bg-black text-white p-4 items-center flex justify-between">
+      <h1 className="font-bold">Team Task Manager</h1>
+      <button onClick={logout} className="bg-red-500 text-white px-4 py-2 rounded">Logout </button>
+    </div>
+  );
 }
 
-export default Navbar
+export default Navbar;
